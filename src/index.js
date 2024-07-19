5f86a878553f692894e14eed0fdb1085b3c780cf
monilshah100 <monilshah100@gmail.com>
2024-07-19 14:47:55 +0530

    innitial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import "./index.css";
-// import App from "./App.jsx";
-
-// ReactDOM.render(
-//   <>
-//     <App />
-//   </>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
